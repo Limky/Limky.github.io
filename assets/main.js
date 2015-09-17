2afd7699fe518b7e05df0d11723908b6f0712d55
Limky <hot8118@naver.com>
2015-09-17 14:58:37 +0900

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,10 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 $(document).ready(function(){
-    $(".tip-top").tooltip({
-        placement : 'top'
-    });
-    $(".tip-right").tooltip({
-        placement : 'right'
-    });
-    $(".tip-bottom").tooltip({
-        placement : 'bottom'
-    });
-    $(".tip-left").tooltip({
-        placement : 'left'
-    });
+    $('[data-toggle="tooltip"]').tooltip();   
 });
+
 
 $(document).ready(function(){
 
