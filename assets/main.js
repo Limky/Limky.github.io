9f5091e971f8720858626485dfde02e11502347c
Limky <hot8118@naver.com>
2015-09-18 14:03:57 +0900

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,28 @@
-$(function() {
-    $( document ).tooltip();
+
+  $(function() {
+    $( "#show-option" ).tooltip({
+      show: {
+        effect: "slideDown",
+        delay: 250
+      }
+    });
+    $( "#hide-option" ).tooltip({
+      hide: {
+        effect: "explode",
+        delay: 250
+      }
+    });
+    $( "#open-event" ).tooltip({
+      show: null,
+      position: {
+        my: "left top",
+        at: "left bottom"
+      },
+      open: function( event, ui ) {
+        ui.tooltip.animate({ top: ui.tooltip.position().top + 10 }, "fast" );
+      }
+    });
   });
-  
   
 $(document).ready(function(){
 	
