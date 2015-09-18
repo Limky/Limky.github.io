73154ce89796d3f058d704ec73483963311edbec
Limky <hot8118@naver.com>
2015-09-18 13:21:33 +0900

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,8 @@
 
 $(document).ready(function(){
 	
-    $(".tip-top").tooltip({
-        placement : 'top'
-    });
-    $(".tip-right").tooltip({
-        placement : 'right'
-    });
-    $(".tip-bottom").tooltip({
-        placement : 'bottom'
-    });
-    $(".tip-left").tooltip({
-        placement : 'left'
-    });
+
+    $('[data-toggle="tooltip"]').tooltip();   
 
     var sel=0;
 	//Window Size setting
